c5fd47ef1a2512c49464b6a0f13f4a82b96f9ec6
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-07-19 22:29:19 +0300

    Added a bit of context to the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,32 @@ import '../App.css';
 
 export default function Home () {
     return (
+        <div>
         <div className="home">
             
-           <h1 className="glass-text">We Know Tea!</h1>
-          
+            
+           <h1 className="glass-text">
+            <em>We Know Tea!</em></h1>
+         
         </div>
+
+        <div className="home-about">
+            <h1>ATLAS TEA BROKERS LIMITED
+                <hr />
+
+            </h1>
+
+             <p>Atlas Tea Brokers, established in 2002, is a reputable tea brokerage company that has quickly 
+                    become a frontrunner in the tea trade. Our profound understanding of the tea industry and commitment
+                     to excellence have made us a preferred choice for both tea producers and buyers. Since joining the 
+                     East African Tea Trade Association (EATTA) in July 2015, we have consistently excelled in our role
+                      as a bridge between producers and buyers, representing 36 prestigious tea gardens from five 
+                      countries. Today, our company commands an impressive 5% market share, a testament to our dedication
+                       in securing the best prices for our clients under prevailing market conditions.</p>
+            
+            
+          </div>
+          </div>
         
     );
 
