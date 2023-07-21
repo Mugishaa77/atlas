cf5a34f74f5111391f53a476652c6b1456e48cde
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-07-21 10:28:40 +0300

    added context to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,25 @@ import '../App.css';
 export default function About () {
     return (
         <div className="about">
-            
+            <h2>ABOUT ATLAS  <hr /></h2>
+            <div className="about-one">
+                <h3> OUR JOURNEY IN NUMBERS <hr /></h3>
+                <ul className="list-container">
+                    <li>Date of Company registration: <span className="num">2002</span> </li>
+
+<li>Date of admission to EATTA :<span className="num">July 2015</span> </li>
+
+<li>Maiden Auction:<span className="num">Sale 39 of September 2015</span> </li>
+
+<li>Number of Marks at the inaugural sale:<span className="num"> 3</span></li>
+
+<li>Number of Gardens today :<span className="num">24 from 3 countries</span></li>
+
+<li>Current market share:<span className="num">5%</span> </li>
+                </ul>
             </div>
+           
+            
+             </div>
     );
 }
