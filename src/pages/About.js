16ac6db73d654dea9d25f38b90a3159ba2a69351
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-07-22 11:17:02 +0300

    Added html to the about us section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,4 @@
+
 import '../App.css';
 
 export default function About () {
@@ -7,21 +8,119 @@ export default function About () {
             <div className="about-one">
                 <h3> OUR JOURNEY IN NUMBERS <hr /></h3>
                 <ul className="list-container">
-                    <li>Date of Company registration: <span className="num">2002</span> </li>
+                    <li>Date of Company registration: <span className="num"> 2002</span> </li>
 
-<li>Date of admission to EATTA :<span className="num">July 2015</span> </li>
+<li>Date of admission to EATTA: <span className="num"> July 2015</span> </li>
 
-<li>Maiden Auction:<span className="num">Sale 39 of September 2015</span> </li>
+<li>Maiden Auction: <span className="num"> Sale 39 of September 2015</span> </li>
 
-<li>Number of Marks at the inaugural sale:<span className="num"> 3</span></li>
+<li>Number of Marks at the inaugural sale: <span className="num"> 3</span></li>
 
-<li>Number of Gardens today :<span className="num">24 from 3 countries</span></li>
+<li>Number of Gardens today: <span className="num"> 24 from 3 countries</span></li>
 
-<li>Current market share:<span className="num">5%</span> </li>
+<li>Current market share: <span className="num"> 5%</span> </li>
                 </ul>
             </div>
            
+            <div className="mission">
+            <h3>OUR MISSION <hr /></h3>
+            {/* h3s should be greeen */}
+            <p>
+                At Atlas Tea Brokers, our mission is to provide top-tier brokerage services to both tea
+producers and buyers, guiding them in navigating the complexities of the tea trade. With our
+in-depth knowledge, unwavering commitment, and personalized approach, we aim to ensure
+that our clients' teas find the right buyers at the Mombasa Auction, securing optimal prices
+for producers and premium products for buyers.
+            </p>
+           </div>
+
+           <div classname="service" >
+            <h3>WHAT WE DO<hr/></h3>
+            <p>As a specialized tea brokerage firm, we offer a comprehensive range of services tailored to
+meet the unique needs of both tea producers and buyers:</p>
+
+<ul><li>
+    <h4>Auction Representation for Producers</h4>
+    <p>We <span>sell teas</span> at the Mombasa Tea Auction under the rules and regulations set by
+EATTA on behalf of our tea producer clients. Our team of experts carefully handles
+the entire auction process, from pre-auction preparation to post-auction follow-ups,
+ensuring seamless transactions and maximum returns for our producer clients.</p></li>
+
+<li>
+    <h4>
+        Sourcing for Buyers</h4>
+        <p>For buyers seeking premium teas, we curate a diverse selection of high-quality teas
+from esteemed <a href="">tea gardens</a>tea gardens. Through our extensive network and market expertise,
+we match buyers with teas that align with their preferences and requirements.</p></li>
+<a href="">View Garden Catalogue</a>
+<li><h4>
+    Private Sales Facilitation</h4>
+    <p>In addition to <span>auction representation</span>, we also <span>facilitate private sales</span> for both
+producers and buyers. Our vast network of international buyers and producers allows
+us to connect parties directly, enabling smooth trade negotiations.</p></li></ul>
             
+
+           </div>
+
+           <div className="values">
+    <h3>OUR VALUES <hr/></h3>
+    <ul><li>
+        <h4>Unmatched Expertise</h4>
+        <p>
+            At Atlas Tea Brokers, <strong>"We Know Tea"</strong> is not just a slogan; it represents the core of our
+identity. Our team comprises seasoned professionals with extensive knowledge of the tea
+industry, including cultivation practices, tea processing, market trends, and pricing dynamics.
+
+This expertise empowers us to provide our clients with well-informed advice and actionable
+insights to help them make strategic decisions.</p></li>
+
+<li>
+    <h4>Elasticity and Adaptability</h4>
+    <p>As the tea industry evolves, so do we. We understand the significance of adaptability and
+remain agile in response to emerging market trends, consumer preferences, and regulatory
+changes. Our flexible approach ensures that our clients, whether producers or buyers, stay
+competitive and seize opportunities in a dynamic market landscape.</p></li>
+
+<li>
+    <h4>Safeguarding Interests</h4>
+    <p>
+        One of our key mandates is to prioritize our clients' interests. We diligently adhere to the
+instructions and terms stipulated by tea producers while assisting buyers in sourcing
+premium teas that match their preferences. Our commitment to securing the highest possible
+price for producers' teas under the prevailing market conditions and providing buyers with
+the finest teas reflects our dedication to the success of all parties involved.</p></li>
+
+<li><h4>Commitment to Sustainability</h4>
+<p>As responsible brokers, we actively advocate for sustainable and eco-friendly practices
+within the tea industry. We partner with socially responsible tea gardens and buyers who
+share our values, supporting their efforts to promote sustainability and fair-trade practices.</p></li>
+
+</ul>
+           </div>
+
+           <div className="garden-catalogue">
+            <h3>GARDEN CATALOGUE</h3>
+            <p>Welcome to our tea wonderland in Kenya, where the magic of nature meets the artistry of tea. </p>
+            <ul>
+                <li>Karirana</li>
+                <li>Maramba</li>
+                <li>Mogeni</li>
+                <li>Nyarinda</li>
+                <li>Kiptegen</li>
+               <li>Magura</li> <li>Arroket</li>
+               <li>Lelagoin</li><li>DL Teas
+                
+               </li>
+<li>Emrok</li>
+<li>Mokong</li>
+<li>Kapchorua</li>
+<li>Tinderet</li><li>Mulindi</li>
+<li>Shagasha</li>
+<li>Pfunda</li>
+<li>Gisakura</li><li>Mata</li><li>Nyabihu</li><li>Rubaya</li><li>Gatare</li><li>Livingstonia</li>
+<li>Mahenye</li><li>Kibena</li>
+            </ul>
+           </div>
              </div>
     );
 }
