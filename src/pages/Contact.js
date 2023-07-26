70aa41da961f161d074869c6c037528bf0ba86eb
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-07-26 15:37:00 +0300

    backup

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeOpenText, faLocationDot, faPhone, faSquarePhone } from "@fortawesome/free-solid-svg-icons";
 import '../App.css';
 
 export default function Contact () {
@@ -25,7 +27,7 @@ export default function Contact () {
             <div className="contact-details">
 <h2>Contact Us</h2>
 <div className="contact-form">
-    <h3>Contact form</h3>
+    <h3>Contact form <FontAwesomeIcon icon={faEnvelopeOpenText} /></h3>
 <form>
     <label>Name:</label>
     <input type="text"
@@ -62,12 +64,50 @@ export default function Contact () {
             </div>
             
             <div className="business-address">
-                Display your company's physical address 
-                so that people can reach out to you through traditional mail or visit your office if needed
+                <h3>Business Address <FontAwesomeIcon icon={faLocationDot} /></h3>
+                <p>Sec, 1 M.Nyali, (Off Links Shell Petrol Station)
+                    <br/>
+                    P.O. Box 3445-80100 Mombasa Kenya
+                </p>
             </div>
 
             <div className="phone-number">
+              <h3>Phone <FontAwesomeIcon icon={faPhone} /></h3>
+              Tel:<a
+                href="tel:0414471226"
+                target="_blank"
+                title="Tel"
+                rel="noopener noreferrer"
+              >
+                
+                <FontAwesomeIcon icon={faSquarePhone} />
+                
+                <span>041 4471226</span>
+              </a>
 
+              Mob:<a
+                href="tel:+254729404044"
+                target="_blank"
+                title="Mob"
+                rel="noopener noreferrer"
+              >
+                
+                <FontAwesomeIcon icon={faSquarePhone} />
+                <span>+254 729 404 044</span>
+              </a>
+
+            </div>
+
+            <div className="email">
+                 <a
+                href="mailto:info@atlastea.co.ke"
+                title="Email"
+                rel="noopener noreferrer"
+                className="email-link"
+              >
+                <FontAwesomeIcon icon={faEnvelope} className="envelope-icon" />
+                <span>info@atlastea.co.ke</span>
+              </a>
             </div>
         </div>
     );
