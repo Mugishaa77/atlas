284d5517da0a7d0b05f4b1d8438e629d03410765
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-07-29 10:21:39 +0300

    Added a directors segment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,6 +2,23 @@ export default function Directors () {
     return (
         <div className="directors">
             <h1>Our Directors</h1>
+            <div className="director">
+                <h2>DR. GEORGE .K. MUHOHO</h2>
+                <h3>Education</h3>
+                <ul>
+                    <li>Urbanian University- Rome (Summa cum Laude) Masters Degree in Canon Law</li>
+                    <li>Urbanian University-Rome Ph.D. (Magna cum Laude) Doctoral thesis traced on the Development of Education
+Policy in Kenya</li>
+<li>“Academia Diplomatic”- Rome Post Graduate Diploma in International Relations& Diplomatic Practice</li>
+                </ul>
+                <h3>Professional Experience</h3>
+                <ul>
+                    <li>Served in the Vatican Embassy in Brussels- Belgian for the Kenya Government with accreditation to Belgium
+Luxembourg, the European Community the Council of Europe and NATO.</li>
+<li>Served in Ministry of Foreign Affairs</li>
+<li></li>
+                </ul>
+            </div>
         </div>
     );
 }
