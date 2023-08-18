6dfefacdb0e688bf386026a1b1dcb8f88a68efbb
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-08-18 12:58:50 +0300

    Made the homepage fully responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,10 @@ import React, { useEffect, useState } from 'react';
 import homepage from '../tea/homepage.png';
 import '../App.css';
 
-export default function Home () {
-    const [bgLoaded, setBgLoaded] = useState(false);
+export default function Home() {
+  const [bgLoaded, setBgLoaded] = useState(false);
 
-
-    useEffect(() => {
+  useEffect(() => {
     const bgImage = new Image();
     bgImage.src = homepage;
     bgImage.onload = () => {
@@ -23,50 +22,45 @@ export default function Home () {
     height: '500px',
     padding: '2rem',
     color: 'var(--atlas-blue)',
-    backgroundImage: bgLoaded ? `url(${homepage})` : 'none', 
+    backgroundImage: bgLoaded ? `url(${homepage})` : 'none',
   };
+
+  // Media Query for screens with max width 900px
   
+ if (window.innerWidth <= 900) {
+  homeStyles.height = '300px'; // Adjusted height for smaller screens
+  homeStyles.padding = '0.5rem'; // Further adjusted padding for smaller screens
+}
+
   return (
-        <div>
-        <div className="home" style={homeStyles}>
-            
-            
-           <h1 className="glass-text">
-            We Know Tea!</h1>
-         
+    <div>
+      <div className="home" style={homeStyles}>
+        <h1 className="glass-text">
+          We Know Tea!
+        </h1>
+      </div>
+
+      <div className="home-about">
+        <h1>ATLAS TEA BROKERS LIMITED<hr /></h1>
+        
+
+        <div className="about-p">
+          <p>
+            <strong>Established in 2002, Atlas Tea Brokers is a Leading Tea Brokerage Company</strong>.<br />
+            As a member of <strong>East Africa Tea Trade Association (EATTA)</strong> since July <strong>2015</strong>, we bridge producers and buyers, representing <strong>36 esteemed tea gardens</strong> across
+            <strong> Kenya, Rwanda, Burundi, Uganda, and Tanzania</strong>.<br />
+            With a strong <strong>7% market share</strong>, we consistently secure optimal prices for our clients.
+          </p>
         </div>
 
-        <div className="home-about">
-            <h1>ATLAS TEA BROKERS LIMITED
-                <hr />
+        <div className="about-button">
+          <Link to="/about">
+            <button>About Us</button>
+          </Link>
+        </div>
+      </div>
 
-            </h1>
-
-            <div className="about-p">
-             <p>
-                
-                    <strong>Established in 2002, Atlas Tea Brokers is a Leading Tea Brokerage Company</strong>.<br/>
-                 As a member of <strong>East Africa Tea Trade Association(EATTA)</strong> since July <strong>2015</strong>, we bridge producers and buyers, representing <strong>36 esteemed tea gardens</strong> across
-                 <strong> Kenya, Rwanda, Burundi, Uganda and Tanzania</strong>.
-                 <br/> With a strong <strong>7% market share</strong>, we consistently secure optimal prices for our clients.
-                </p>            </div>
-
-                 <div className="about-button">
-                    <Link to="/about">
-                         <button>About Us</button>
-                    </Link>
-                   
-                 </div>
-            
-          </div>
-         
-
-          
-          
-          <div className="footer"></div>
-
-          </div>
-        
-    );
-
+      <div className="footer"></div>
+    </div>
+  );
 }
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-
+import '../App.css';
 import Logo from '../tea/Logo.PNG';
 
 export default function Navbar() {
