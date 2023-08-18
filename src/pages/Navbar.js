557b35110631321a7bb0dc4085b6e5417065701f
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-08-18 12:28:43 +0300

    Made the navbar fully mobile responsive

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,30 +1,48 @@
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
 import Logo from '../tea/Logo.PNG';
-import '../App.css';
-import { Link } from 'react-router-dom';
 
-export default function Navbar () {
-    return (
-      
+export default function Navbar() {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  const location = useLocation(); // Get the current location from React Router
 
-<div className="Navbar">
-            <nav>
-  <div className="logo">
-    <Link to="/">
-  <img src={Logo} width="200" height="65px" alt=""/>
-  </Link>
-  </div>
-  <ul>
-    <li><a href="/">HOME</a></li>
-    <li><a href="/about">ABOUT US</a></li>
-    <li><a href="/service">SERVICES</a></li>
-    <li><a href="/news">NEWS</a></li>
-    <li><a href="/auction">TEA AUCTION</a></li>
-    <li><a href="/contact">CONTACT US</a></li>
-  </ul>
-</nav>
+  const handleMenuToggle = () => {
+    setIsMenuActive(!isMenuActive);
+  };
 
+  const isActive = (pathname) => {
+    return location.pathname === pathname ? { color: 'var(--atlas-green)' } : {};
+  };
+
+  return (
+    <div className="Navbar">
+      <nav>
+        <div className="logo">
+          <Link to="/">
+            <img
+              src={Logo}
+              width="200"
+              height="65px"
+              alt="Atlas Tea Brokers Ltd, Tea Brokerage Company in Mombasa, Kenya representing Kenya, Rwanda, Burundi, Kenya, and Tanzania in the EATTA"
+              title="Atlas Tea Brokers Ltd, Kenya"
+            />
+          </Link>
         </div>
-
-       
-    );
+        <div className={`menu-toggle ${isMenuActive ? 'active' : ''}`} onClick={handleMenuToggle}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <ul className={`nav-links ${isMenuActive ? 'active' : ''}`}>
+          <li><Link to="/" style={isActive('/')} >HOME</Link></li>
+          <li><Link to="/about" style={isActive('/about')}>ABOUT US</Link></li>
+          <li><Link to="/service" style={isActive('/service')}>SERVICES</Link></li>
+          <li><Link to="/news" style={isActive('/news')}>NEWS</Link></li>
+          <li><Link to="/auction" style={isActive('/auction')}>TEA AUCTION</Link></li>
+          <li><Link to="/contact" style={isActive('/contact')}>CONTACT US</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
