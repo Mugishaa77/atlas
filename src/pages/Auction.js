7c648a169a4d9ea7d99ebcf440070c6cbb93b881
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-08-29 11:13:22 +0300

    created the tea auction page

--- src/pages/Auction.js
+++ src/pages/Auction.js
@@ -1,9 +1,42 @@
 import '../App.css';
+import { NavLink, Outlet } from 'react-router-dom';
+
 
 export default function Auction () {
     return (
         <div className="auction">
             <h2>TEA AUCTION <hr/></h2>
+
+            <div className="side-nav">
+                <h3>
+                    <strong>QUICK LINKS</strong></h3>
+                <NavLink className={({ isActive }) =>
+                isActive ? "link-active" : "link-inactive"
+            }
+                to ="/">Auction Reports</NavLink>
+
+                <NavLink className={({ isActive }) =>
+                isActive ? "link-active" : "link-inactive"
+            }
+                to ="/">Auction Results</NavLink>
+
+                <NavLink className={({ isActive }) =>
+                isActive ? "link-active" : "link-inactive"
+            }
+                to ="/">Market Reports</NavLink>
+
+                <NavLink className={({ isActive }) =>
+                isActive ? "link-active" : "link-inactive"
+            }
+                to ="/">Forthcoming Auctions</NavLink>
+
+
+            </div>
+
+            <div className="content">
+                <Outlet />
+
+            </div>
         </div>
     );
 }
