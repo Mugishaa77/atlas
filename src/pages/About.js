702dce0fe9616a98fcfdafbf3c20cc948d4858a2
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-08-31 13:08:52 +0300

    fixing the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import Directors from '../components/Directors';
-import ImageSlideshow from '../components/ImageSlideshow';
+import karirana from '../estates/karirana-one.jpg';
+import maramba from '../estates/maramba-one.jpg';
 import '../App.css';
 
 export default function About() {
@@ -13,7 +14,7 @@ export default function About() {
                             <li>Date of Company registration: <span className="num">2002</span></li>
                             <li>Date of admission to EATTA: <span className="num">July 2015</span></li>
                         </div>
-                        <div className="figure">
+                       <div className="figure">
                             <li>Maiden Auction: <span className="num">Sale 39 of September 2015</span></li>
                             <li>Number of Marks at the inaugural sale: <span className="num">3</span></li>
                         </div>
@@ -38,10 +39,11 @@ export default function About() {
                     <p><em>We Know Tea</em></p>
                 </div>
 
-                <div className="koisagat">
-                   <ImageSlideshow />
+                <div className="journey">
+                    
+                </div>
+
                 
-            </div>
             </div>
 
             <div className="mission">
@@ -60,6 +62,22 @@ export default function About() {
             <div className="directors-component">
                 <Directors />
             </div>
+
+            <div className="conclusion">
+                <h3>CONCLUSION<hr/>
+                </h3>
+                <p>
+                   
+ We invite <span>tea producers and buyers</span> alike to <span>partner with us</span> on their <span>journey to success</span> as we navigate
+the intricacies of the tea trade together, securing the <span>best prices for producers</span> and <span>delivering
+premium teas to buyers.</span>
+<br />
+<span>At Atlas Tea Brokers, we are dedicated allies in your pursuit of
+excellence in the world of tea.</span>
+ 
+                </p>
+                
+                </div>
         </div>
     );
 }
