1e08779f80e5e40edcf2a3b1f54991acd1ca7c6d
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-09-13 11:18:39 +0300

    finalized on the ui of tea catalogues

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -16,30 +16,102 @@ const Catalog = () => {
                         Auction 36
                         
     
-                        </li></ul></div></li>
+                        </li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div></ul></div></li>
             <li><h5>August</h5>
             <div className="month">
                 <ul>
-                    <li>Auction 35</li>
-                    <li>Auction 34</li>
-                    <li>Auction 33</li>
-                    <li>Auction 32</li>
-                    <li>Auction 31</li>
+                    <li>Auction 35</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 34</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 33</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 32</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 31</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
                 </ul>
             </div>
             </li>
             <li><h5>July</h5>
             <div className="month">
                 <ul>
-                    <li>Auction 30</li>
-                    <li>Auction 29</li>
-                    <li>Auction 28</li>
-                    <li>Auction 27</li></ul></div></li>
+                    <li>Auction 30</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 29</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 28</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 27</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div></ul></div></li>
             <li><h5>June</h5>
             <div className="month">
                 <ul>
-                    <li>Auction 26</li>
-                    <li>Auction 25</li>
+                    <li>Auction 26</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
+                    <li>Auction 25</li><div className="button">
+                          <button className="view" title="preview document">View</button>
+                        <button className="download" title="download file"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+                        </div>
                 </ul>
             </div>
            </li>
@@ -47,9 +119,88 @@ const Catalog = () => {
 
         </ul>
         </div>,
-    <div>Page 2 Content</div>,
-    <div>Page 3 Content</div>,
-    <div>Page 4 Content</div>
+    <div className="last-year">
+  <h4>2022</h4>
+  <div className="month-list">
+    <ul>
+      <li><h5>December</h5></li>
+      <li><h5>November</h5></li>
+      <li><h5>October</h5></li>
+      <li><h5>September</h5></li>
+    </ul>
+  </div>
+  <div className="month-list">
+    <ul>
+      <li><h5>August</h5></li>
+      <li><h5>July</h5></li>
+      <li><h5>June</h5></li>
+      <li><h5>May</h5></li>
+    </ul>
+  </div>
+  <div className="month-list">
+    <ul>
+    <li><h5>April</h5></li>
+      <li><h5>March</h5></li>
+      <li><h5>February</h5></li>
+      <li><h5>January</h5></li></ul>
+  </div>
+</div>
+,
+<div className="last-year">
+  <h4>2021</h4>
+  <div className="month-list">
+    <ul>
+      <li><h5>December</h5></li>
+      <li><h5>November</h5></li>
+      <li><h5>October</h5></li>
+      <li><h5>September</h5></li>
+    </ul>
+  </div>
+  <div className="month-list">
+    <ul>
+      <li><h5>August</h5></li>
+      <li><h5>July</h5></li>
+      <li><h5>June</h5></li>
+      <li><h5>May</h5></li>
+    </ul>
+  </div>
+  <div className="month-list">
+    <ul>
+    <li><h5>April</h5></li>
+      <li><h5>March</h5></li>
+      <li><h5>February</h5></li>
+      <li><h5>January</h5></li></ul>
+  </div>
+</div>,
+<div className="last-year">
+  <h4>2020</h4>
+  <div className="month-list">
+    <ul>
+      <li><h5>December</h5></li>
+      <li><h5>November</h5></li>
+      <li><h5>October</h5></li>
+      <li><h5>September</h5></li>
+    </ul>
+  </div>
+  <div className="month-list">
+    <ul>
+      <li><h5>August</h5></li>
+      <li><h5>July</h5></li>
+      <li><h5>June</h5></li>
+      <li><h5>May</h5></li>
+    </ul>
+  </div>
+  <div className="month-list">
+    <ul>
+    <li><h5>April</h5></li>
+      <li><h5>March</h5></li>
+      <li><h5>February</h5></li>
+      <li><h5>January</h5></li></ul>
+  </div>
+</div>
+  
+    
+
     // Add more pages as needed
   ];
 
