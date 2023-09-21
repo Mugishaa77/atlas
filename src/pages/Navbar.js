9c6d296c82797502836f645c9105be0cd68faf31
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-09-21 12:23:02 +0300

    Added the gallery to the navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -40,6 +40,7 @@ export default function Navbar() {
           <li><Link to="/service" style={isActive('/service')}>SERVICES</Link></li>
           <li><Link to="/news" style={isActive('/news')}>NEWS</Link></li>
           <li><Link to="/auction" style={isActive('/auction')}>TEA AUCTION</Link></li>
+          <li><Link to="/garden" style={isActive('/garden')}>GALLERY</Link></li>
           <li><Link to="/contact" style={isActive('/contact')}>CONTACT US</Link></li>
         </ul>
       </nav>
