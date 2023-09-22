d9e3baba81a8d3d6eeab98d02034a52618abed92
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-09-22 11:04:40 +0300

    connected the front end to the backend

--- src/components/Weekly.js
+++ src/components/Weekly.js
@@ -1,20 +1,24 @@
-import { useState, useEffect, useRef  } from 'react';
+import { useState  } from 'react';
 import './Weekly.css';
 
 function Weekly () {
+
+  // Define your URLs with the fallback to localhost:5000
+const baseUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
+
   const pdfFiles = [
-  '/reports/Market_Report_Sale_35.pdf',
-  '/reports/Market_Report_Sale_34.pdf',
-  '/reports/Market_Report_Sale_33.pdf',
-  '/reports/Market_Report_Sale_32.pdf',
-  '/reports/Market_Report_Sale_31.pdf',
-  '/reports/Market_Report_Sale_30.pdf',
-  '/reports/Market_Report_Sale_29.pdf',
-  '/reports/Market_Report_Sale_28.pdf',
-  '/reports/Market_Report_Sale_27.pdf',
-  '/reports/Market_Report_Sale_26.pdf',
-  '/reports/Market_Report_Sale_25.pdf',
-  '/reports/Market_Report_Sale_24.pdf',
+  `${baseUrl}/reports/Market_Report_Sale_35.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_34.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_33.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_32.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_31.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_30.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_29.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_28.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_27.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_26.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_25.pdf`,
+  `${baseUrl}/reports/Market_Report_Sale_24.pdf`,
   // Add more URLs as needed
 ];
 
@@ -64,7 +68,8 @@ return (
                         <div className="month-report">
                             <ul>
                             {month.report.map((report, reportIndex) => (
-                                <li key={reportIndex}>
+                               <li key={reportIndex}>
+
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
 </svg>{report}
@@ -75,6 +80,8 @@ return (
                           target="_blank"
                           rel="noopener noreferrer"
                           title="preview document"
+                          
+
                         >
                           View
                         </a>
@@ -87,9 +94,10 @@ return (
                             <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
                           </svg>
                         </a>
-                      </div>
+                      </div> 
                                 </li>
                             ))}
+                            
                             </ul>
 
                         </div>
