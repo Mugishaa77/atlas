f9c339622aa4c5e40209c82a1e3c72f606d5b9e9
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-10-26 13:27:46 +0300

    renamed files

--- src/documents/Industry.js
+++ src/documents/Industry.js
@@ -1,1 +1,5 @@
-import january from './industry/'
+import october from './industry/FINAL TEA INDUSTRY PERFORMANCE REPORT OCTOBER 2022 (1).pdf';
+import july from './industry/KENYA TEA INDUSTRY PERFORMAMCE03937620221019120202-1.pdf';
+import june from './industry/KENYA TEA INDUSTRY PERFORMANCE-JUNE.pdf';
+import april from './industry/KENYA TEA INDUSTRY TEA PERFORMANCE~A05526620230703110939.pdf';
+import february from './industry/KENYA TEA INDUSTRY PERFORMANCE - FEBRUARY.pdf';
