cecf548c66ccc83d25e385add4c44643f520f24f
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-10-28 23:19:37 +0300

    Styled Tea Science

--- src/documents/Industry.js
+++ src/documents/Industry.js
@@ -37,7 +37,7 @@ const [showButtons, setShowButtons] = useState(Array(reportTitles.length).fill(f
     
     return (
       <div className="industry-reports">
-      <h2>Tea Industry Reports
+      <h2>Tea Industry Reports 
        
       </h2>
       <div >
@@ -46,12 +46,11 @@ const [showButtons, setShowButtons] = useState(Array(reportTitles.length).fill(f
           <li key={reportIndex}>
             <div className="report">
               <h5 onClick={() => toggleButtons(reportIndex)}>{report.name}
-               <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
   <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
 </svg>
-                </span></h5>
+               </h5>
               {showButtons[reportIndex] && (
                 <div className="button">
                   <a
@@ -61,7 +60,10 @@ const [showButtons, setShowButtons] = useState(Array(reportTitles.length).fill(f
                     rel="noopener noreferrer"
                     title="preview document"
                   >
-                    View
+                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
+</svg>
+                    view
                   </a>
                   <a
                     className="download"
@@ -69,11 +71,12 @@ const [showButtons, setShowButtons] = useState(Array(reportTitles.length).fill(f
                     download
                     title="download file"
                   >
-                    Download
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
-                      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                      <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cloud-download" viewBox="0 0 16 16">
+  <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
+  <path d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
+</svg>
+                    download
+                    
                   </a>
                 </div>
               )}
--- src/pages/about/Science.js
+++ src/pages/about/Science.js
@@ -1,41 +1,45 @@
 import scienceOne from '../../tea/science-one.jpg';
 import scienceTwo from '../../tea/science-two.jpg';
 import Industry from '../../documents/Industry';
+import teaProduction from '../../tea/tea-production.png';
+import Footer from '../../components/Footer';
+import './AboutPage.css';
 
 export default function Science () {
     return (<div className="science">
         <div className="tbk-reports">
+            <div className="i-one">
             <Industry />
+            </div>
+            <div className="i-two">
+
+            </div>
         </div>
         <div className="structure">
+            <h2>Tea Science</h2>
+            <div className="tea-science-pics">
             <img src={scienceOne}/>
             <img src={scienceTwo}/>
+            </div>
 
-             <p> As tea enthusiasts and industry experts, we understand the importance
-                 of quality at every stage of tea production.</p>
-                 import React from 'react';
+            <div className="tea-production">
+                <h2>Production Statistics ~ Kenya </h2>
+                <img src={teaProduction} />
+                <a href="https://www.statista.com/statistics/1154187/tea-production-in-kenya/"
+                rel="noopener noreferrer"
+                target="_blank" ></a>
 
-    <div>
-      <h2>Exploring Tea Quality</h2>
+            </div>
 
-      <p>Welcome to our Tea Quality section, where we delve into the fascinating world of tea cultivation and the pursuit of exceptional tea. Below, you'll find visual insights into the very heart of tea: the tea plant cell and a high-quality tea leaf.</p>
+            
 
-      <div>
-             <p><strong>Tea Plant Cell:</strong> Take a microscopic journey into the intricate world of a tea plant cell. Discover the remarkable structure and components that contribute to the unique character of each tea leaf.</p>
-      </div>
-
-      <div>
-           <p><strong>High-Quality Tea Leaf:</strong> An exquisite tea begins with the finest leaves. Explore the characteristics of a superior tea leaf, from its appearance to the promise of a remarkable flavor and aroma.</p>
-      </div>
-
-      <p>As tea enthusiasts and industry experts, we understand the importance of quality at every stage of tea production. Join us in our quest for the perfect cup of tea, and learn more about the dedication and expertise that go into our tea brokerage services.</p>
-
-      <p>Discover the art and science of tea quality with Atlas Tea Brokers Ltd.</p>
-    </div>
-  
+    
 
 
        
+            </div>
+            <div>
+                <Footer />
             </div>
     </div>)
 }
