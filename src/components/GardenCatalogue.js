22229a3416d0e8b73c1d1a9f01274acc240eef39
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-11-02 14:36:06 +0300

    Remaining: gallery

--- src/components/GardenCatalogue.js
+++ src/components/GardenCatalogue.js
@@ -1,5 +1,3 @@
-import ImageList from '../components/ImageList';
-import Pictures from '../components/Pictures';
 import './Garden.css';
 
 export default function GardenCatalogue () {
@@ -7,14 +5,7 @@ export default function GardenCatalogue () {
     return (
         <div className="garden">
             <h2>AT A GLANCE</h2>
-            <div className="pictures">
-            <Pictures />
-        </div>added
-                    
-            <div className="logos">
-            <ImageList />
-        </div>
-        
+                
             
             
         
