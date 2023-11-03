0aaf5cd937fdf2c7ea8b2f35cc661ebdcdb56958
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-11-03 13:29:10 +0300

    Added hd photos to the wbsite

--- src/components/GardenCatalogue.js
+++ src/components/GardenCatalogue.js
@@ -1,14 +1,15 @@
 import './Garden.css';
-import Plantations from '../components/Plantations';
 import Pictures from '../components/Pictures';
+import Footer from '../components/Footer';
 
 export default function GardenCatalogue () {
    
     return (
         <div className="garden">
             <h2>AT A GLANCE</h2>
-            <Plantations />
+    
             <Pictures />
+            <Footer />
                 
             
             
