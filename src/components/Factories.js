8c3b8b5124b4a7947349ad08a4a81324dd4b57ee
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-11-05 22:17:07 +0300

    oraginizing factories

--- src/components/Factories.js
+++ src/components/Factories.js
@@ -1,25 +1,55 @@
 import gatare from '../tea/gatare.png';
-
 import mbogo from '../ttea/mbogo-valley.png';
-import maramba from '../ttea/maramba-tea.png';
-import sorwathe from '../ttea/sorwathe.png';
 import pfunda from '../ttea/pfunda.png';
 import mulindii from '../ttea/mulindi-factory.png';
 import kisaru from '../ttea/kisaru-f.png';
 import koisagat from '../ttea/Koisagat.png';
+// new
+import emrok from '../pictures/EMROK TEA FACTORY LTD(EPZ).jpg';
+import gisakura from '../pictures/Gisakura Tea Factory.jpg';
+import kipkebe from '../pictures/KIPKEBE TEA LTD.jpg';
+import kkisaru from '../pictures/KISARU TEA ESTATE.jpg';
+import kitumbe from '../pictures/KITUMBE TEA FACTORY.jpg';
+import korara from '../pictures/KORARA HIGHLANDS TEA FACTORY.jpg';
+import maramba from '../pictures/MARAMBA TEA FACTORY.jpg';
+import mata from '../pictures/MATA TEA COMPANY LTD.jpg';
+import mogeni from '../pictures/MOGENI TEA FACTORY.jpg';
+import kivu from '../pictures/muganza-kivu tea factory.jpg';
+import mulindi from '../pictures/MULINDI FACTORY COMPANY LTD.jpg';
+import otb from '../pictures/OTB.jpg';
+import sasini from '../pictures/SASINI.jpg';
+import  shagasha from '../pictures/SHAGASHA TEA FACTORY.jpg';
+import sorwathe from '../pictures/SORWATHE TEA FACTORY.jpg';
+
 import './Garden.css';
 
 const factories = [
-{ image: koisagat, title: 'DL-Koisagat Tea Estate Ltd,Kenya', caption: 'DL Koisagat Tea Estate Ltd, Kenya' },
-
-   {image: pfunda, title: 'Pfunda Tea Company, Rubavu, Rwanda', caption: 'Pfunda Tea Company, Rwanda'},
+{ image: koisagat, title: 'DL-Koisagat Tea Estate Ltd,Kenya' },
+   {image: pfunda, title: 'Pfunda Tea Company, Rwanda'}, 
+    {image:sasini, title: 'Sasini' },   
+ {image: mulindii, title: 'Mulindi Tea Factory, Rwanda'},
+  {image: kivu, title: 'Muganza-Kivu Tea Factory'},
+  {image: kisaru, title:'Kisaru Tea Factory, Uganda'},
+       {image: mulindi, title: 'Mulindi Factory Company Ltd'},
+  {image: sorwathe, title: 'Sorwathe Tea Factory'},
+     {image: korara, title: 'Korara Highlands Tea Factory'},   
+  {image: mbogo, title: 'Mbogo Valley Tea Factory, Kenya'},
+  { image: gatare, title: 'Gatare Tea Company Ltd, Rwanda' },
+  // new
+ 
+      {image: shagasha, title: 'Shagasha Tea Factory' },
+     
+       {image: kipkebe, title: 'Kipkebe Tea Ltd'},
+      {image: kkisaru, title: 'Kisaru Tea Estate'},
+      {image: kitumbe, title: 'Kitumbe Tea Factory'},
+      
+      {image: maramba, title: 'Maramba Tea Factory'},
+      {image: mata, title: 'Mata Tea Company Ltd'},
+      {image: mogeni, title: 'Mogeni Tea Factory'},
+      {image: emrok, title: 'Emrok Tea Factory (EPZ)'},
+       {image:gisakura, title: 'Gisakura Tea Factory'},
+        {image: otb, title: 'OTB'},
     
- {image: mulindii, title: 'Mulindi Factory Company- The Wood Foundation, Rwanda', caption: 'Mulindi Tea Factory, Rwanda'},
-  {image: maramba, title: 'Maramba Tea Factrory, Limuru/Banana Road', caption: 'Maramba Tea Factory, Kenya'},
-  {image: kisaru, title:'Kisaru Tea Factory, Uganda', caption: 'Kisaru Tea Factory, Uganda'},
-  {image: sorwathe, title: 'Sorwathe Tea Factory, Kinihira, Rwanda', caption: 'Sorwathe Tea Factory, Rwanda'},
-  { image: gatare, title: 'Gatare Tea Company Ltd, Rwanda Mountain Tea, Rwanda', caption: 'Gatare Tea Company Ltd, Rwanda' },
-  {image: mbogo, title: 'Mbogo Valley Tea Factory, Kenya', caption: 'Mbogo Valley Tea Factory, Kenya'},
 
     
 ]
@@ -36,7 +66,7 @@ export default function Factories() {
              alt={factories.title}
              title={factories.title}
              loading="lazy" />
-             <p>{factories.caption}</p>
+             <p>{factories.title}</p>
           </li>
         ))}
       </ul>
--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,21 +1,3 @@
-import koisagat from  '../pictures/DL KOISAGAT TEA FACTORY.jpg';
-import emrok from '../pictures/EMROK TEA FACTORY LTD(EPZ).jpg';
-import gatare from '../pictures/GATARE TEA COMPANY LTD.jpg';
-import gisakura from '../pictures/Gisakura Tea Factory.jpg';
-import kipkebe from '../pictures/KIPKEBE TEA LTD.jpg';
-import kisaru from '../pictures/KISARU TEA ESTATE.jpg';
-import kitumbe from '../pictures/KITUMBE TEA FACTORY.jpg';
-import korara from '../pictures/KORARA HIGHLANDS TEA FACTORY.jpg';
-import maramba from '../pictures/MARAMBA TEA FACTORY.jpg';
-import mata from '../pictures/MATA TEA COMPANY LTD.jpg';
-import mogeni from '../pictures/MOGENI TEA FACTORY.jpg';
-import kivu from '../pictures/muganza-kivu tea factory.jpg';
-import mulindi from '../pictures/MULINDI FACTORY COMPANY LTD.jpg';
-import nyabihu from '../pictures/NYABIHU TEA FACTORY.jpg';
-import otb from '../pictures/OTB.jpg';
-import sasini from '../pictures/SASINI.jpg';
-import  shagasha from '../pictures/SHAGASHA TEA FACTORY.jpg';
-import sorwathe from '../pictures/SORWATHE TEA FACTORY.jpg';
 
  
 
@@ -26,25 +8,7 @@ export default function Pictures() {
      
        
 // factories
-           {image:sasini, title: 'Sasini' },
-      {image: shagasha, title: 'Shagasha Tea Factory' },
-      {image: sorwathe, title: 'Sorwathe Tea Factory'},
-      {image: nyabihu, title: 'Nyabihu Tea Factory'},
-      {image: kivu, title: 'Muganza-Kivu Tea Factory'},
-      {image: mulindi, title: 'Mulindi Factory Company Ltd'},
-      {image: otb, title: 'OTB'},
-       {image: kipkebe, title: 'Kipkebe Tea Ltd'},
-      {image: kisaru, title: 'Kisaru Tea Estate'},
-      {image: kitumbe, title: 'Kitumbe Tea Factory'},
-      {image: korara, title: 'Korara Highlands Tea Factory'},
-      {image: maramba, title: 'Maramba Tea Factory'},
-      {image: mata, title: 'Mata Tea Company Ltd'},
-      {image: mogeni, title: 'Mogeni Tea Factory'},
-      {image: koisagat, title: 'DL Koisagat Tea Factory' },
-      {image: emrok, title: 'Emrok Tea Factory (EPZ)'},
-       {image: gatare, title: 'Gatare Tea Company Ltd'},
-       {image:gisakura, title: 'Gisakura Tea Factory'},
-    
+           
     ]
   return (
     <div className="plantations">
