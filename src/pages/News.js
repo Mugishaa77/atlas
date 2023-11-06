11fe990fb1122000d333ea58725db1fb86dbdb21
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-11-06 12:33:55 +0300

    Added context to the News Page

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,8 +1,24 @@
 import '../News.css';
 import DateTimeDisplay from '../components/DateTimeDisplay';
 import audit from '../tea/aaudit.jpg';
+import React, { useState, useEffect } from 'react';
+
 
 export default function News () {
+    const [lastUpdated, setLastUpdated] = useState(new Date().toLocaleString());
+    
+    useEffect(() => {
+    // Update the lastUpdated value when your content changes
+    // For example, when a data fetch or other updates occur
+    // You can call setLastUpdated with the new date and time
+    const updateLastUpdated = () => {
+      setLastUpdated(new Date().toLocaleString());
+    };
+
+    // Call the updateLastUpdated function whenever your content changes
+    updateLastUpdated();
+    }, []);
+
     return (
          <div className="news">
            <h2>NEWS<hr/></h2>
@@ -19,7 +35,10 @@ export default function News () {
                 production costs that are cutting into farmers' earnings.
                 <br/>
                 Purpose of the audit is to help safeguard the interests of small shareholder farmers and gains in the industry.
-            
+            <br/>
+           <span>
+             Last Updated: {lastUpdated}
+           </span>
                 </p>
 
                 < a href="https://nation.africa/kenya/business/kenya-tea-value-chain-for-audit-on-high-production-costs-4424382"
