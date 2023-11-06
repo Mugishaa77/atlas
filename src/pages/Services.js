399dbd5c3b56a98bbabf040264ae339f851e14b3
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-11-06 14:11:18 +0300

    Improved quality of maps

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,17 +1,15 @@
 import {Link} from 'react-router-dom';
-import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Maps from '../components/Maps';
 
-export default function Services() {
-     const [currentPage, setCurrentPage] = useState(0);
+export default function Services() {  
 
-  const pages = [
+  
+    return (      
 
-    <div>
-    <Maps /> </div>,
-    <>
-    <div className="service">
+        <div className="services">
+            <h2>OUR SERVICES<hr/></h2>    
+             <div className="service">
                
                 <p className="open">As a specialized tea brokerage firm, we offer a comprehensive range of services 
                 tailored to meet the unique needs of both tea producers and buyers:</p>
@@ -71,51 +69,15 @@ export default function Services() {
                     </div>
                 </div>
             </div>
-             
+              <Maps />   
             <div className="service-close">
                 <h3>COMMITMENT TO SUSTAINABILITY<hr/></h3>
                 <p>As responsible brokers, we <span>actively advocate</span> for <strong>sustainable</strong> and <strong>eco-friendly</strong> practices within the tea industry.<br/> We partner with <strong>socially
                      responsible</strong> tea gardens and buyers who share our values, supporting their efforts to promote
                       <span> sustainability</span> and <span>fair-trade practices</span>.</p>
-            </div></>,
-            
-
-    
-
-    // Add more pages as needed
-  ];
-
-  const nextPage = () => {
-    if (currentPage < pages.length - 1) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const prevPage = () => {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-  
-    return (
-        
-
-        <div className="services">
-            <h2>OUR SERVICES<hr/></h2>           
-            <div>
-                 {pages[currentPage]}
             </div>
-              <div className="catalog-controls">
-        <button onClick={prevPage} disabled={currentPage === 0}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-  <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-</svg> Previous</button>
-        <span>{`Page ${currentPage + 1} of ${pages.length}`}</span> {/* Displays page number */}
-        <button onClick={nextPage} disabled={currentPage === pages.length - 1}>Next <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
-  <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg></button>
-      </div>
+               
+           
     
 
 
